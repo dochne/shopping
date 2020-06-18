de0e01f04117a1a003de47619309e92e56218347
Doug Nelson <dougnelson@silktide.com>
2020-06-18 14:45:01 +0100

    reformatted the output

--- src/Service/PrintService.php
+++ src/Service/PrintService.php
@@ -18,10 +18,54 @@ class PrintService
         $this->categoryRepository = $categoryRepository;
     }
 
+    private function optimalLines(array $items, int $lineLength = 42) : array
+    {
+        $output = [];
+        $line = [];
+        foreach ($items as $item) {
+            if (count($line) === 0) {
+                $line[] = $item;
+                continue;
+            }
+
+            // Remember we need at least one space!
+            if (strlen($line[0]) + strlen($item) + 1 > $lineLength) {
+                $output[] = $line;
+                $line = [$item];
+                continue;
+            }
+
+            $line[] = $item;
+
+            if (count($line) === 2) {
+                $output[] = $line;
+                $line = [];
+                continue;
+            }
+        }
+
+        if (count($line) > 0) {
+            $output[] = $line;
+        }
+
+        $outputStrings = [];
+        foreach ($output as $line) {
+            if (!isset($line[1])) {
+                $outputStrings[] = $line[0];
+                continue;
+            }
+
+            $string = str_pad($line[0], $lineLength - strlen($line[1])) . $line[1];
+            $outputStrings[] = $string;
+        }
+        return $outputStrings;
+    }
+
+
     public function print() : bool
     {
-        //$connector = new RemotePrintConnector();
-        $connector = new FilePrintConnector("/dev/usb/lp0");
+        $connector = new RemotePrintConnector();
+        //$connector = new FilePrintConnector("/dev/usb/lp0");
         $printer = new Printer($connector);
 
         $categories = array_filter($this->categoryRepository->all(), function(Category $category) {
@@ -32,23 +76,45 @@ class PrintService
             return false;
         }
 
+        date_default_timezone_set("Europe/London");
+
         // Todo: possibly fingerprint devices so we know who printed it
         $printer->setJustification(Printer::JUSTIFY_RIGHT);
         $printer->setTextSize(1, 1);
+        $printer->selectPrintMode(Printer::MODE_EMPHASIZED);
         $printer->text( date("Y-m-d H:i") . "\n");
+        $printer->selectPrintMode(Printer::MODE_FONT_A);
 
         foreach ($categories as $category) {
             $printer->setJustification(Printer::JUSTIFY_CENTER);
             $printer->setTextSize(2, 2);
-            $printer->text("\n" . $category->name . "\n\n");
+            $printer->selectPrintMode(Printer::MODE_EMPHASIZED);
+            $printer->text($category->name . "\n");
+            $printer->selectPrintMode(Printer::MODE_FONT_A);
 
             $printer->setJustification(Printer::JUSTIFY_LEFT);
-            $printer->setTextSize(1, 1);
+            //$printer->setTextSize(1, 1);
             $trimmed = $category->shopping;
             $lines = explode("\n", $trimmed);
-            foreach ($lines as $line) {
-                $printer->text($line . "\n");
+
+            foreach ($this->optimalLines($lines) as $string) {
+                $printer->text($string . "\n");
             }
+//
+//            foreach ($lines as $n => $line) {
+//                if ($n % 2 === 0) {
+//                    $printer->setJustification(Printer::JUSTIFY_LEFT);
+//                    $printer->text($line);
+//                } else {
+//                    $printer->setJustification(Printer::JUSTIFY_RIGHT);
+//                    $printer->text($line . "\n");
+//                }
+//            }
+
+
+
+
+
         }
 
         $printer->cut();
